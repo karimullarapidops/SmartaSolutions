90df73aa90eb1fdb004e677e86dd56b684532512
VAMSI AILA <35575642+vamsiaila@users.noreply.github.com>
2018-04-13 20:03:44 +0530

    nav and slider updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { SliderComponent } from './slider/slider.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule
