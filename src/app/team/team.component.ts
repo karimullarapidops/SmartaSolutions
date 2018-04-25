04895978c5d28c29b83a7a3255354a983be2e6c2
VAMSI AILA <35575642+vamsiaila@users.noreply.github.com>
2018-04-25 22:35:28 +0530

    team

--- src/app/team/team.component.ts
+++ src/app/team/team.component.ts
@@ -12,4 +12,11 @@ export class TeamComponent implements OnInit {
   ngOnInit() {
   }
 
+  team=[
+    { 'name':'Kay Garland', 'position':'Lead Designer', 'picture':'../../assets/images/1.jpg' },
+    { 'name':'Larry Parker', 'position':'Lead Marketer', 'picture':'../../assets/images/2.jpg' },
+    { 'name':'Diana Pertersen', 'position':'Lead Developer', 'picture':'../../assets/images/3.jpg' }
+  ];
+
+
 }
