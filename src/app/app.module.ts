bb96a54c8d06c604c2cf185be90e21f10426b28b
VAMSI AILA <35575642+vamsiaila@users.noreply.github.com>
2018-04-30 12:29:50 +0530

    sample error messages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
@@ -60,6 +61,7 @@ import { WorkdayContentComponent } from './workday-content/workday-content.compo
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FormsModule,
     MatButtonModule,
     MatInputModule,
     MatSelectModule,
