e0e08fea8d7320bcb0ce9284a876957c2155aceb
VAMSI AILA <35575642+vamsiaila@users.noreply.github.com>
2018-05-01 12:39:30 +0530

    trigger update

--- src/app/animate.ts
+++ src/app/animate.ts
@@ -1,6 +1,6 @@
 import { trigger, state, style, transition, animate } from "@angular/animations";
 
-export let fade = trigger('fade',[
+export let slide = trigger('slide',[
 
 transition(':enter',[
     style({transform:'translateY(-500px)'}),
