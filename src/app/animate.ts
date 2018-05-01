b5144f6525c59ae01a5ede27d9a8475376b01f91
VAMSI AILA <35575642+vamsiaila@users.noreply.github.com>
2018-05-01 21:11:58 +0530

    min animate.ts

--- src/app/animate.ts
+++ src/app/animate.ts
@@ -1,18 +1,30 @@
-import { trigger, state, style, transition, animate, query, animateChild } from "@angular/animations";
+import { trigger, state, style, transition, animate, query, animateChild, animation, useAnimation } from "@angular/animations";
+
+// re-usable animations starts
+
+export let slideAnimation = animation([
+    style({opacity:0, transform:'translateX(-100%)'}),
+    animate(500)
+]);
+
+export let zoomInAnimation = animation([
+    style({opacity:0, transform:'scale(3,3)'}),
+    animate(500)
+]);
+
+// re-usable animation ends
 
 export let slide = trigger('slide',[
-
 transition(':enter',[
     query('#extras',[
     style({transform:'translateY(-500px)'}),
     animate('0.6s cubic-bezier(.64,-0.42,.52,1.36)')
     ]),
     query('@zoomin',animateChild())
-])
+    ])
 ]);
 
 export let slideh = trigger('slideh',[
-
     transition(':enter',[
         query('#home',[
         style({transform:'translateY(-500px)'}),
@@ -24,42 +36,16 @@ export let slideh = trigger('slideh',[
         query('@fadeas',animateChild()),
         query('@fadeb',animateChild())
     ])
-    ]);
+]);
 
+export let fades = trigger('fades',[transition(':enter', useAnimation(slideAnimation))]);
 
-export let fades = trigger('fades',[
-    transition(':enter',[
-        style({opacity:0, transform:'translateX(-100%)'}),
-        animate(500)
-    ])
-])
-export let fadeap = trigger('fadeap',[
-    transition(':enter',[
-        style({opacity:0, transform:'translateX(-100%)'}),
-        animate(500)
-    ])
-])
-export let fadeg = trigger('fadeg',[
-    transition(':enter',[
-        style({opacity:0, transform:'translateX(-100%)'}),
-        animate(500)
-    ])
-])
-export let fadeas = trigger('fadeas',[
-    transition(':enter',[
-        style({opacity:0, transform:'translateX(-100%)'}),
-        animate(500)
-    ])
-])
-export let fadeb = trigger('fadeb',[
-    transition(':enter',[
-        style({opacity:0, transform:'translateX(-100%)'}),
-        animate(500)
-    ])
-])
-export let zoomin = trigger('zoomin',[
-    transition(':enter',[
-        style({opacity:0, transform:'scale(3,3)'}),
-        animate(500)
-    ])
-])
+export let fadeap = trigger('fadeap',[transition(':enter', useAnimation(slideAnimation))]);
+
+export let fadeg = trigger('fadeg',[transition(':enter', useAnimation(slideAnimation))]);
+
+export let fadeas = trigger('fadeas',[transition(':enter', useAnimation(slideAnimation))]);
+
+export let fadeb = trigger('fadeb',[transition(':enter', useAnimation(slideAnimation))]);
+
+export let zoomin = trigger('zoomin',[transition(':enter', useAnimation(zoomInAnimation))]);
